ec6f5825bc794a17c209271cf33e145324dad3d2
helpdeskng <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>
2022-06-10 11:56:20 -0700

    executing program

--- command_line_to_av.c
+++ command_line_to_av.c
@@ -7,21 +7,20 @@
  */
 int main(void)
 {
-	char *buffer = malloc(1024);
-	size_t len = 1024;
-	char *token;
-
-	while (1)
-	{
-		printf("$ ");
-		getline(&buffer, &len, stdin);
-		printf("buffer getline: %s", buffer);
-	}
-	token = strtok(buffer, " ");
-	while (token)
-	{
-		printf("buffer strtok: %s\n", token);
-		token = strtok(NULL, " ");
-	}
-	return (0);
+char *buffer = malloc(1024);
+size_t len = 1024;
+char *token;
+while (1)
+{
+printf("$ ");
+getline(&buffer, &len, stdin);
+printf("buffer getline: %s", buffer);
+}
+token = strtok(buffer, " ");
+while (token)
+{
+printf("buffer strtok: %s\n", token);
+token = strtok(NULL, " ");
+}
+return (0);
 }
